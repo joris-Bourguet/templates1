f73569cb7f9d97ea49061342d47a18603ded4ae0
joris-Bourguet <43138838+joris-Bourguet@users.noreply.github.com>
2018-12-05 08:54:50 +0100

    erreur validator corrigé

--- tarif.js
+++ tarif.js
@@ -6,6 +6,6 @@ function calcul(tarif)
     var enfant3 = parseInt(tarif.enfant3.value);
     
     var tarif = (adulte * 50) + (enfant1 * 35) + (enfant2 * 40) + (enfant3 * 45);
-    window.document.getElementById('Prix').innerHTML = "Le prix a payer est de : " + tarif +" euros par ans";
+    window.document.getElementById('Prix').innerHTML = "Le totale a payer est de : " + tarif +" euros par ans";
     
 }
