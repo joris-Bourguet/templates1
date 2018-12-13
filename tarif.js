39ff218822ed937078bb8c2e13ba4cb0472b09bb
joris-Bourguet <43138838+joris-Bourguet@users.noreply.github.com>
2018-12-13 20:56:14 +0100

    finission

--- tarif.js
+++ tarif.js
@@ -27,7 +27,15 @@ function myMap() {
         map : map
     })
 }
+  
 
-function valider_formulaire(){
-    window.alert('validation');
-}
+  /*  var myForm = document.getElementById('myForm');
+
+    myForm.addEventListener('submit', function(e) {
+
+        alert('Vous avez envoyé le formulaire !\n\nMais celui-ci a été bloqué pour que vous ne changiez pas de page.');
+    
+        e.preventDefault();
+    
+    });
+*/
